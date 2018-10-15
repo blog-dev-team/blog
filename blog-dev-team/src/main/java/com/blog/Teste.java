42f5195ab0087660b94902e6269bca39cbacdd2a
victor.godoy <victor.godoy@vmengsoft02>
2018-10-15 17:03:31 -0300

    teste

---
+++ blog-dev-team/src/main/java/com/blog/Teste.java
@@ -0,0 +1,13 @@
+package com.blog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Teste {
+
+	@GetMapping("teste")
+	public String get() {
+		return "funciona mesmo";
+	}
+}
